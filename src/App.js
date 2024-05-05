12846b9cf6085948115a06ec757a4a0eade64d54
Jyoti Santani <jyotisantani@Jyotis-MacBook-Air.local>
2024-05-06 02:22:56 +0530

    scroll-update

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import Home from "./routes/home";
 import About from "./routes/about";
 import Contact from "./routes/contact";
 import Project from "./routes/project";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return <>
+  <ScrollToTop />
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/project" element={<Project />} />
