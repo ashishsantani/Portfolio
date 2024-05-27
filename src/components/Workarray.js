6badbcfdd3accce54245822688ee364a200c26ac
Jyoti Santani <jyotisantani@Jyotis-MacBook-Air.local>
2024-05-27 18:55:56 +0530

    new project

--- src/components/Workarray.js
+++ src/components/Workarray.js
@@ -3,8 +3,10 @@ import img2 from "./images/MineDog.png"
 import img3 from "./images/parthivPortfolio.png"
 import img4 from "./images/ashCourseBundler.png"
 import img5 from "./images/ashEcommerce.png"
+import img6 from "./images/expense-tracker.png"
 import { SiExpress, SiMongodb, SiReact } from "react-icons/si";
 import { BiLogoHtml5, BiLogoCss3, BiLogoJavascript, BiLogoNodejs } from "react-icons/bi";
+import { FaSquareJs } from "react-icons/fa6";
 import { FaBootstrap } from "react-icons/fa"
 
 const workitems = [
@@ -83,6 +85,22 @@ const workitems = [
         content: "Created a full-stack MERN (MongoDB, Express.js, React.js, Node.js) comprehensive ecommerce website aimed at delivering a seamless shopping experience to users. Designed and implemented an admin dashboard enabling CRUD (Create, Read, Update, Delete) operations",
         viewbtn: "https://ashecommerceclone.vercel.app",
         sourcebtn: "https://github.com/ashishraja/Ecommerce-clone-frontend"
+    },
+    {
+        id: 6,
+        imgsrc: img6,
+        icons: <>
+            <BiLogoHtml5 color="red" size={"30px"} style={{ margin: "0 8px" }} />
+            <BiLogoCss3 color="blue" size={"30px"} style={{ margin: "0 8px" }} />
+            <SiMongodb color="green" size={"30px"} style={{ margin: "0 8px" }} />
+            <SiExpress color="yellow" size={"30px"} style={{ margin: "0 8px" }} />
+            <BiLogoNodejs color="green" size={"30px"} style={{ margin: "0 8px" }} />
+            <FaSquareJs color="yellow" size={"30px"} style={{ margin: "0 8px" }} />
+        </>,
+        title: "Expense Tracker",
+        content: "Developed an Expense Tracker Application using Node.js, Express, EJS, HTML, and CSS. The application provides a user-friendly interface for easy expense input and tracking, with real-time updates and total amount calculation. Key feature is intuitive and responsive UI design using HTML, CSS, and EJS templating engine for seamless user experience.",
+        viewbtn: "https://https://expense-tracker-umg4.onrender.com/",
+        sourcebtn: "https://github.com/ashishraja/expense-tracker"
     },
 ];
 export default workitems;
